e21712317ae11a51853f61f50c7f747ea6fa19e4
Kathryn <kathryn.chu10@gmail.com>
2019-04-27 12:52:28 -0700

    Add register and login route for Users

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // The callback for every Express route requires a request
 // and response as arguments
@@ -10,6 +12,7 @@ router.get('/test', (req, res) => res.json({
   msg: 'This is the users route',
 }));
 
+// User sign-up
 router.post('/register', (req, res) => {
 
   // Check to make sure nobody has already registered with a duped email
@@ -33,12 +36,69 @@ router.post('/register', (req, res) => {
             if (err) throw err;
             newUser.password = hash;
             newUser.save()
-              .then(user => res.json(user))
+              .then(user => {
+                const payload = { id: user.id, name: user.name };
+
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                });
+              })
               .catch(err => console.log(err));
           });
         });
       }
     });
+
+
 });
+
+// User log-in
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({
+          email: 'This user does not exist'
+        });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            // res.json({ msg: 'Success' });
+
+            const payload = {
+              id: user.id,
+              name: user.name
+            };
+
+            // We want to return a signed web token with each login or register 
+            // request in order to "sign the user in" on the frontend.
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success:true,
+                  token: 'Bearer ' + token
+                });
+              }
+            );
+
+          } else {
+            return res.status(400).json({ password: 'Incorrect password'});
+          }
+        });
+    });
+});
+
 
 module.exports = router;
